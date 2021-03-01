c9c3e9978ea20cfb13ac0fcc4e17f108033c20f0
Tilman Raendchen <traendchen@outlook.com>
2021-03-01 02:52:32 +0000

    Fix domain collection

--- web/domains/index.js
+++ web/domains/index.js
@@ -1,1 +1,7 @@
-module.exports = global.index(__dirname);
+const fs = require("fs");
+
+let routers = {};
+for (const route of fs.readdirSync(__dirname).filter(x => x !== "index.js"))
+	routers[route] = require(join(__dirname, route, "index.js"));
+
+module.exports = routers;
