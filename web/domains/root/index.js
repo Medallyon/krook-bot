43a0b9326087d5af954908dd6aba61bca1f85a98
Tilman Raendchen <traendchen@outlook.com>
2021-03-24 21:39:22 +0000

    Fix /web newline welcome string

--- web/domains/root/index.js
+++ web/domains/root/index.js
@@ -8,7 +8,7 @@ router.set("view engine", "ejs");
 /* GET / */
 router.get("/", function(req, res)
 {
-	res.send("Hello World :)\nJoin our cool discord server: <a href='https://discord.gg/zUdwuCU'>https://discord.gg/zUdwuCU</a>");
+	res.send("Hello World :)<br>Join our cool discord server: <a href='https://discord.gg/zUdwuCU'>https://discord.gg/zUdwuCU</a>");
 });
 
 module.exports = router;
