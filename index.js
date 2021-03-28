343fd5e8b395f95aac6c6ea8c82a0490d460a3dc
Tilman Raendchen <traendchen@outlook.com>
2021-03-28 18:29:16 +0100

    Combine server & client once again

--- index.js
+++ index.js
@@ -5,9 +5,9 @@ require("./globals.js");
 const Client = require("./lib/classes/Client.js");
 let client = new Client();
 
-client.login(process.env.BOT_TOKEN)
+client.login(process.env.DISCORD_BOT_TOKEN)
 	.catch(console.error);
 
-// let server = require("./web");
+let server = require("./web");
 
-module.exports = { client/*, server*/ };
+module.exports = { client, server };
