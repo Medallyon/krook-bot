32533659765e78de77e3cba1aed1b081f9a74846
Tilman Raendchen <traendchen@outlook.com>
2021-04-08 03:38:42 +0100

    Fix twitch live event

--- web/domains/api/webhooks/index.js
+++ web/domains/api/webhooks/index.js
@@ -1,35 +1,37 @@
 const router = require("express").Router()
-	, Queue = require("bull")
-	, twitch = new (require(join(__webdir, "classes", "Twitch.js")))();
-
-const announceQueue = new Queue("twitch-announce", process.env.REDIS_URL || "redis://127.0.0.1:6379");
+	, Cache = require("node-cache");
 
 router.get("/", function(req, res)
 {
 	res.sendStatus(200);
 });
 
-const twitchIDs = [];
+const twitchIDs = new Cache({
+	stdTTL: 60
+});
+
 router.post("/", async function(req, res)
 {
-	if (twitchIDs.includes(req.get("Twitch-Eventsub-Message-Id")))
+	if (twitchIDs.has(req.get("Twitch-Eventsub-Message-Id")))
 		return res.sendStatus(204);
+	twitchIDs.set(req.get("Twitch-Eventsub-Message-Id"));
 
 	if (req.body && req.body.challenge)
 		return res.send(req.body.challenge);
 	else
 		res.sendStatus(200);
 
-	await twitch.init();
+	const event = req.body.event
+		, { client } = require(join(__basedir, "index.js"));
+	await client.twitch.init();
 
-	const event = req.body.event;
-	twitch.users.fetch(event.broadcaster_user_id)
+	client.twitch.users.fetch(event.broadcaster_user_id)
 		.then(streamer =>
 		{
-			twitch.streams.fetch(event.broadcaster_user_id)
+			client.twitch.streams.fetch(event.broadcaster_user_id)
 				.then(stream =>
 				{
-					announceQueue.add({ event, streamer, stream });
+					client.emit("twitch-live", { event, streamer, stream });
 				}).catch(console.error);
 		}).catch(console.error);
 });
